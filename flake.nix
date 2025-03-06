967f99e94f2b86db7abacbce647a2fb2f1b1137e
Iris System <iris@iris.ac.nz>
2025-03-07 10:37:17 +1300

    update flake stuff

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   outputs = { flake-parts, ... } @ inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
+      systems = [ "x86_64-linux" "aarch64-linux" ];
       perSystem = { self', pkgs, ... }: {
-        devShells.default = pkgs.mkShell {
+        devShells.backend = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             pkg-config
           ];
@@ -21,11 +21,21 @@
             librsvg
           ];
         };
+
+        devShells.web = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+
+          buildInputs = with pkgs; [
+            nodejs
+          ];
+        };
       };
     };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
   };
